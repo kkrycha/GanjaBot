6a5c42c8ae36676f48fd3f0cfff51346e8765fe4
kkrycha <se4@interia.pl>
2016-05-21 05:04:07 +0100

    Zmiana angielskich tekstów bota na polskie

--- plugin_command_timebomb.py
+++ plugin_command_timebomb.py
@@ -4,8 +4,8 @@
 #
 #  Magnet2 is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or     
-#  (at your option) any later version.                                   
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
 #
 #  Magnet2 is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -19,11 +19,11 @@ import random, time
 from magnet_api import *
 from magnet_utils import *
 
-WIRE_COLORS = ("red", "orange", "yellow", "green", "blue", "indigo",
-  "violet", "black", "white", "grey", "brown", "pink", "mauve", "beige",
-  "aquamarine", "chartreuse", "bisque", "crimson", "fuchsia", "gold",
-  "ivory", "khaki", "lavender", "lime", "magenta", "maroon", "navy",
-  "olive", "plum", "silver", "tan", "teal", "turquoise")
+WIRE_COLORS = ("czerwony", "pomaranczowy", "zolty", "zielony", "niebieski", "indygo",
+  "fioletowy", "czarny", "bialy", "szary", "helupa", "rozowy", "pedalski", "lososiowy",
+  "wodny", "sraki", "mietowy", "pawi", "fuksja", "zloty",
+  "kebabowy", "khaki", "gin", "limonka", "magenta", "jasnawy", "ciemnawy",
+  "oliwkowy", "śliwkowy", "łyzeczkowy", "wieczorny", "ogniowy", "turkus")
 
 lastbombed = {}
 timebombed = {}
@@ -35,17 +35,17 @@ def timer_timebomb(bot, arg):
       if bot.in_roster(room, nick):
         if timebombed[room]['dud']:
           bot.send_room_message(room,
-            "%s is lucky! The bomb was dud!"%(nick))
+            "%s Pechowiec! Bombka się nie władowała!"%(nick))
         else:
           bot.send_room_message(room,
-            "%s has failed to cut the wire in time! The bomb explodes!"%(nick))
+            "%s Nie zdąrzył zarzucić! Faza go rozjebała!"%(nick))
       else:
         if timebombed[room]['dud']:
           bot.send_room_message(room,
-            "Looks like %s ran away with a dud bomb."%(nick))
+            "Wygląda na to, ze %s znow lamusiarsko odmawia fazy."%(nick))
         else:
           bot.send_room_message(room,
-            "You hear %s explode somewhere far away."%(nick))
+            "Słyszysz jak %s dolewa resztki giebela łkając cichutko"%(nick))
       del timebombed[room]
 
 timebomb_timer = TimedEventHandler(timer_timebomb, 2)
@@ -59,21 +59,21 @@ def event_nick_changed(bot, (presence, room, nick, newnick)):
 def command_timebomb(bot, room, nick, access_level, parameters, message):
   if not parameters: return 'Target expected.'
   if message.getType() != 'groupchat':
-    return 'Sneaky! This works only in the groupchat.'
+    return 'Sprytnie w pizde! Ale dziala tylko gdy wszyscy wiedza'
   if room in timebombed:
     if nick == timebombed[room]['nick']:
-      return 'You have to take care of your own first.'
-    return 'Only one victim at a time!'
+      return 'Zajmij sie wpierw soba i swoimi kablami cpaku '
+    return ' Jedna bomba jeden cpunix '
   target = parameters
   if target[-1] == ' ': target = target[0:-1]
   if target == bot.self_nick[room]:
-    return "Don't even think about it."
+    return "Nawet tego nie odpierdalaj.."
   if target == nick:
-    return 'I am not designed to help in committing suicide.'
+    return 'Zapomnij! Pomoge Ci sie zabic, Ty sobie spokojnie bedziesz lezal i gnil a mnie rozmontuja? Zapomnij!'
   if not bot.in_roster(room, target):
-    return "I don't see %s here."%(target)
+    return "Prawdopodobnie %s sie odcial i nic nie widzi."%(target)
   if room in lastbombed and target in lastbombed[room]:
-    return 'Give them a break! Pick someone else.'
+    return 'Podziel dragi i daj mu odetchnac przed nastepna faza !'
 
   bombtimer = random.randint(15, 60)
   bombtime = time.time() + bombtimer
@@ -90,20 +90,20 @@ def command_timebomb(bot, room, nick, access_level, parameters, message):
     'wires': bombwires
   }
   return (
-    "/me stuffs the bomb into %s's pants. The display reads %d seconds.\r\n"+
-    "Defuse the bomb by cutting the correct wire (!cutwire color).\r\n"+
-    "There are %d wires: %s."
+    "/me wrzucil towar %s's do kieszeni. Towar straci swa moc za %d sekund.\r\n"+
+    "Zmien swoj stan walac towiec w odpowiedni kabel(!cutwire color).\r\n"+
+    "Masz do wyboru %d kolorowych kabli gotowych do przyjecia: %s."
   )%(target, bombtimer, len(bombwires), ', '.join(bombwires))
 
 
 def command_cutwire(bot, room, nick, access_level, parameters, message):
   if not room in timebombed:
-    return 'No bomb here but your imagination.'
+    return 'Przestan cpac tu nikt nie ma bomby.'
   if not parameters: return 'Color expected.'
   if message.getType() != 'groupchat':
-    return 'Sneaky! This works only in the groupchat.'
+    return 'Sprytnie w chuj! Ale dziala tylko jak widza wszyscy'
   if nick != timebombed[room]['nick']:
-    return '%s is the only hope!'%(timebombed[room]['nick'])
+    return '%s zaraz wyrwie z papci!'%(timebombed[room]['nick'])
   wire = parameters.lower()
   if wire[-1] == ' ': wire = wire[0:-1]
   if wire in timebombed[room]['wires']:
@@ -112,26 +112,26 @@ def command_cutwire(bot, room, nick, access_level, parameters, message):
     result = random.randint(1, 3)
     if not timebombed[room]['wires']: result = 2
     if result == 1:
-      return "That wire didn't do anything!"
+      return " Tym kablem nie wejdzie!"
     elif result == 2:
       timeleft = timebombed[room]['time']-time.time()
       if timeleft > 2:
-        res = "You defused the bomb with %d seconds remaining!"%(timeleft)
+        res = "Hahah! Zbiłeś sobie banie %d sekund przed jej wejsciem !"%(timeleft)
       else:
-        res = "You defused the bomb just in the last moment!"
+        res = " W ostatnim momencie pozbawiles sie dobrej bani "
       del timebombed[room]
       return res
     else:
       del timebombed[room]
-      return "Wrong wire! The bomb explodes!"
+      return "Zly kabel! Rozjebales se serce!"
   else:
-    return 'There is no such wire colored "%s"!'%(wire)
+    return 'Nie ma "%s" kabla ktorego szukasz !'%(wire)
 
 def command_defuse(bot, room, nick, access_level, parameters, message):
   if not room in timebombed:
-    return 'No bomb here but your imagination.'
+    return 'Przestan cpac tu nikt nie ma bomby.'
   del timebombed[room]
-  return 'The bomb has been defused.'
+  return 'Yebac admina yebac!'
 
 def load(bot):
   bot.add_command('timebomb', command_timebomb, LEVEL_GUEST, 'timebomb')
@@ -141,6 +141,6 @@ def load(bot):
 
 def unload(bot):
   bot.timed_events.remove(timebomb_timer)
-  
+
 def info(bot):
   return 'Timebomb plugin v1.0.1'
